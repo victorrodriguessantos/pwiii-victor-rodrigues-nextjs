643e8244d8da61de87f601e12cbf1da95ba98dcf
Victor <victorrodrigues288@gmail.com>
2025-06-13 20:48:22 -0300

    Configurando o portifolio

---
+++ meu-portifolio/app/components/Principal.tsx
@@ -0,0 +1,11 @@
+export default function Principal() {
+    return (
+        <section className="bg-sky-700 w-full h-dvh p-10">
+            <h1 className="text-4xl text-blue-50">Olá, eu sou o <span className="text-amber-600 font-bold">Victor Rodrigues :)</span></h1>
+            <p className="text-xl mt-10 text-blue-50">Desenvolvedor FullStack e DevOps</p>
+
+            <button className="bg-blue-500 text-blue-50 w-40 h-10 rounded-2xl">Baixar Curriculo</button>
+            <button className="bg-blue-500 text-blue-50 w-40 h-10 ml-10 mt-30 rounded-2xl">Entrar em Contato</button>
+        </section>
+    )
+}
--- meu-portifolio/app/page.tsx
+++ meu-portifolio/app/page.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import Navbar from '\components\Navbar';
+import Navbar from './components/Navbar';
+import Principal from './components/Principal';
 
 export default function Home() {
   return (
@@ -9,25 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Navbar />
-      <main className="p-6">
-        <section id="sobre" className="mb-10">
-          <h1 className="text-3xl font-bold">Olá! Eu sou [Seu Nome]</h1>
-          <p className="mt-2 text-gray-700">Desenvolvedor em formação. Amo criar coisas com código!</p>
-        </section>
-
-        <section id="projetos" className="mb-10 bg-gray-100 p-6 rounded">
-          <h2 className="text-2xl font-semibold mb-4">Meus Projetos</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-white p-4 shadow rounded">Projeto 1</div>
-            <div className="bg-white p-4 shadow rounded">Projeto 2</div>
-          </div>
-        </section>
-
-        <section id="contato">
-          <h2 className="text-2xl font-semibold mb-2">Contato</h2>
-          <p>Você pode me encontrar no email: seuemail@example.com</p>
-        </section>
-      </main>
+      <Principal />
     </>
   );
 }
